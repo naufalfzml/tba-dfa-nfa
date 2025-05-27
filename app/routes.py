0646cb33877b2c4b8cb6a79d2129875942e53a4c
Bryant Cianata <164145534+Cianat@users.noreply.github.com>
2025-05-27 21:06:52 +0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from app.logic.regex_to_nfa import RegexToNFA
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/regex-to-nfa', methods=['GET', 'POST'])
+def regex_to_nfaview():
+    nfa_description = None
+    regex_input = ""
+    if request.method == 'POST':
+        regex_input = request.form.get('regex', '')
+        converter = RegexToNFA()
+        nfa = converter.regex_to_nfa(regex_input)
+        nfa_description = converter.get_nfa_description(nfa)
+    return render_template('regex_to_nfa.html', nfa=nfa_description, regex=regex_input)
