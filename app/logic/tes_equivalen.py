ad46d5b38eb27f36e94bbd40ca11bd59c1eb53d5
hellenaurelia <hellenaurelia@student.uns.ac.id>
2025-05-28 14:20:20 +0700

    ekuivalen2

--- app/logic/tes_equivalen.py
+++ app/logic/tes_equivalen.py
@@ -1,27 +1,43 @@
-from automata.fa.dfa import DFA
+from collections import deque
 
-def cek_ekuivalensi(dfa1_data, dfa2_data):
-    try:
-        # Bangun DFA 1
-        dfa1 = DFA(
-            states=set(dfa1_data['states']),
-            input_symbols=set(dfa1_data['input_symbols']),
-            transitions=dfa1_data['transitions'],
-            initial_state=dfa1_data['initial_state'],
-            final_states=set(dfa1_data['final_states'])
-        )
+def parse_dfa(states, symbols, start, finals, transitions):
+    state_set = set(states.split(','))
+    symbol_set = set(symbols.split(','))
+    final_set = set(finals.split(','))
+    transition_lines = transitions.strip().split('\n')
+    
+    transition_dict = {state: {} for state in state_set}
+    for line in transition_lines:
+        from_state, symbol, to_state = line.strip().split(',')
+        transition_dict[from_state][symbol] = to_state
+    return {
+        'states': state_set,
+        'symbols': symbol_set,
+        'start': start,
+        'finals': final_set,
+        'transitions': transition_dict
+    }
 
-        # Bangun DFA 2
-        dfa2 = DFA(
-            states=set(dfa2_data['states']),
-            input_symbols=set(dfa2_data['input_symbols']),
-            transitions=dfa2_data['transitions'],
-            initial_state=dfa2_data['initial_state'],
-            final_states=set(dfa2_data['final_states'])
-        )
+def are_equivalent(dfa1, dfa2):
+    visited = set()
+    queue = deque([(dfa1['start'], dfa2['start'])])
 
-        # Cek ekivalensi
-        return dfa1.minify() == dfa2.minify(), None
+    while queue:
+        s1, s2 = queue.popleft()
+        if (s1, s2) in visited:
+            continue
+        visited.add((s1, s2))
 
-    except Exception as e:
-        return False, str(e)
+        is_final_1 = s1 in dfa1['finals']
+        is_final_2 = s2 in dfa2['finals']
+        if is_final_1 != is_final_2:
+            return False
+
+        for symbol in dfa1['symbols']:
+            t1 = dfa1['transitions'].get(s1, {}).get(symbol)
+            t2 = dfa2['transitions'].get(s2, {}).get(symbol)
+            if t1 is None or t2 is None:
+                continue
+            queue.append((t1, t2))
+
+    return True
